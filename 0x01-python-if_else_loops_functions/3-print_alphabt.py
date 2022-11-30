f85fc259de11bc6d9da96314420b43c8d049bbe4
Mahamat Abdelkerim Abakar <mahmatabdelkerim863@gmail.com>
2022-11-30 09:04:58 +0000

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for n in range(97, 123):
-    if n == 101 or n == 113:
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print("{}".format(chr(n)), end="")
+    else:
+        print("{:s}".format(chr(i)), end="")
