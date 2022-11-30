bb7c423623cbac104ecc043ba32393b7034e5ffa
Mahamat Abdelkerim Abakar <mahmatabdelkerim863@gmail.com>
2022-11-30 09:32:50 +0000

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
+        change = 0
+        if ord(i) > 96 and ord(i) < 123:
+            change = 32
+        print("{:s}".format(chr(ord(i) - change)), end="")
     print()
