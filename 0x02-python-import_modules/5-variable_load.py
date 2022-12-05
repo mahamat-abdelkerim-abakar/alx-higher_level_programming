c333324a0a071e9648e85e65459d95cd7f7b04cf
Mahamat Abdelkerim Abakar <mahmatabdelkerim863@gmail.com>
2022-12-05 16:28:56 +0000

    0x02. Python - import & modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,10 @@
-
 #!/usr/bin/python3
+from variable_load_5 import a
+
+
+def find_var():
+    print(a)
+
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
+    find_var()
