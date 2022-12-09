2b0b333dd9793305494f6a6f52ba00860cca34d9
Mahamat Abdelkerim Abakar <mahmatabdelkerim863@gmail.com>
2022-12-09 16:11:28 +0000

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,4 @@
-#!/usr/bin/python3 def update_dictionary(a_dictionary, key, value):     a_dictionary[key] = value     return (a_dictionary) 
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return (my_dict)
