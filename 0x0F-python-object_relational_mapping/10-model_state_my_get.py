ff72981d972958d52da361232bbb870c951f72dd
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2023-03-21 23:56:32 +0530

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-# prints the State object with name input
+"""
+prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa
+"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    from sys import argv
-    from sqlalchemy.engine.url import URL
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
-
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-
-    sobj = (argv[4], )
-    try:
-        state = session.query(State).filter(State.name == sobj).one_or_none()
-        print("{}".format(state.id))
-    except:
+    for state in session.query(State):
+        if sys.argv[4] == state.name:
+            print("{}".format(state.id))
+            break
+    else:
         print("Not found")
