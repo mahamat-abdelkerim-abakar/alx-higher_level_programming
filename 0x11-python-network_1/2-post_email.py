3cdd202eb69111d3090b7a4e2c7f493c881a2841
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2023-05-10 12:11:59 +0530

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
-=======
 """ Script that takes in a URL and an email, sends a POST request
  And displays the body of the response
 """
 
->>>>>>> 822dd0bfabd8921df412878fcde8e70da9d11087
 import sys
 import urllib.parse
 import urllib.request
@@ -22,10 +14,5 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
-<<<<<<< HEAD
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
-=======
     with urllib.request.urlopen(request) as resp:
         print(resp.read().decode("utf-8"))
->>>>>>> 822dd0bfabd8921df412878fcde8e70da9d11087
